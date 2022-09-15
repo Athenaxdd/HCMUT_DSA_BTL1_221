2ae2309163bbc05cefca9ea1de588354d663ec62
Athenaxdd <binchu7410@gmail.com>
2022-09-16 00:33:18 +0700

    initial commit

---
+++ ConcatStringList.h
@@ -0,0 +1,72 @@
+#ifndef __CONCAT_STRING_LIST_H__
+#define __CONCAT_STRING_LIST_H__
+
+#include "main.h"
+
+class CharArrayList {
+private:
+    int maxSize;
+    int currentSize;
+    int currentElement;
+    string * list;
+
+public:
+    CharArrayList(int size = 256){
+        maxSize = size;
+        currentSize = currentElement = 0;
+        list = new string[maxSize];
+    }
+};
+
+class ConcatStringList {
+public:
+    class ReferencesList; // forward declaration
+    class DeleteStringList; // forward declaration
+
+public:
+    static ReferencesList refList;
+    static DeleteStringList delStrList;
+
+    // TODO: may provide some attributes
+
+    int currentNode;
+    int maxSize;
+    int listSize;
+
+
+public:
+
+    ConcatStringList(const char *){
+        
+    };
+    int length() const{
+        return listSize;
+    };
+    int get(int index) const;
+    int indexOf(char c) const;
+    std::string toString() const;
+    ConcatStringList concat(const ConcatStringList & otherS) const;
+    ConcatStringList subString(int from, int to) const;
+    ConcatStringList reverse() const;
+    ~ConcatStringList();
+
+public:
+    class ReferencesList {
+        // TODO: may provide some attributes
+
+        public:
+            int size() const;
+            int refCountAt(int index) const;
+            std::string refCountsString() const;
+    };
+
+    class DeleteStringList {
+        // TODO: may provide some attributes
+
+        public:
+            int size() const;
+            std::string totalRefCountsString() const;
+    };
+};
+
+#endif // __CONCAT_STRING_LIST_H__
