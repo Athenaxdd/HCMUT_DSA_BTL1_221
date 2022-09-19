d3632243afb187ee4c9ce75e8e80ef9e3b924e5a
Athenaxdd <binchu7410@gmail.com>
2022-09-19 10:12:29 +0700

    add indexOf, toString, get

--- ConcatStringList.h
+++ ConcatStringList.h
@@ -3,59 +3,84 @@
 
 #include "main.h"
 
+//g++ -o main main.cpp ConcatStringList.cpp -I . -std=c++11
+
 class charArrayList {
 private:
     int maxSize;
     int size;
-    int currentElement;
-    string * list;
+    int current;
+    char * list;
 
 public:
     charArrayList(int size = 256){
         maxSize = size;
-        size = currentElement = 0;
-        list = new string[maxSize];
+        size = current = 0;
+        list = new char[maxSize];
     }
     ~charArrayList(){
         delete [] list;
     }
     void clear(){
         delete [] list;
-        size = currentElement = 0;
-        list = new string[maxSize];
+        size = current = 0;
+        list = new char[maxSize];
     }
-    void toStart(){
-        currentElement = 0;
+    void toBegin(){
+        current = 0;
     }
     void toEnd(){ 
-        currentElement = size;
+        current = size;
     }
     void next(){
-        if (currentElement < size) currentElement++;
+        if (current < size) current++;
     }
     void prev(){
-        if(currentElement != 0) currentElement--;
+        if(current != 0) current--;
     }
     int length() const{
         return size;
     }
+    void append(const char& s){
+        list[size++] = s;
+    }
+    void resize(int size){
+        maxSize = size;
+        list = new char[maxSize];
+    }
+    void insert (const char& s){
+        for (int i = size; i > current; --i){
+            list[i] = list[i-1];
+        }
+            list[current] = s;
+            size++;
+    }
     int currentPosition() const{
-        return currentElement;
+        return current;
     }
     void moveToPosition(int pos){
-        if ((pos >= 0) && (pos <= size)) {
-            cout << "Out of range";
+        if ((pos <= 0) && (pos >= size)) {
             return;
         }
-        else currentElement = pos;
+        else current = pos;
+    }
+    char& getValue() {
+        return list[current];
     }
 };
 
 class charALNode{
 public:
-    charArrayList node;
-    charALNode next;
-    
+    charArrayList nodeData;
+    charALNode * next;
+    charALNode(const char *s) {
+        int size = 0;
+        while(s[size] != '\0') size++;
+        for (int i = 0; i < size; ++i){
+            nodeData.append(s[i]);
+        }
+        this -> next = nullptr;
+    }
 };
 
 class ConcatStringList {
@@ -68,22 +93,55 @@ public:
     static DeleteStringList delStrList;
 
     // TODO: may provide some attributes
+    charALNode* head = nullptr;
+    charALNode* tail = nullptr;
+    int listSize = 0;
 
 public:
-
-    ConcatStringList(const char *){
-        
+    ConcatStringList(const char *s){
+        head = new charALNode(s);
+        tail = head;
+        listSize += head -> nodeData.length();
     };
-    // int length() const{
-    //     return listSize;
-    // };
-    int get(int index) const;
-    int indexOf(char c) const;
-    std::string toString() const;
-    ConcatStringList concat(const ConcatStringList & otherS) const;
-    ConcatStringList subString(int from, int to) const;
-    ConcatStringList reverse() const;
-    ~ConcatStringList();
+    int length() const{
+        return listSize;
+    };
+    char get(int index) const{
+        if (index > listSize || index < 0) throw out_of_range("Index of string is invalid!");
+        charALNode * ptr = head;
+        int travel = 0;
+        while (travel + ptr -> nodeData.length() <= index){
+            travel += ptr -> nodeData.length();
+            ptr = ptr ->next;
+        }
+        ptr ->nodeData.moveToPosition(index - travel);
+        return ptr -> nodeData.getValue();
+    };
+    int indexOf(char c) const{
+        charALNode * ptr = head;
+        int position = 0;
+        while(!ptr){
+            if(ptr -> nodeData.getValue() == c) return position;
+            else {
+                position++;
+                ptr = ptr -> next;
+            }
+        }
+        return -1;
+    };
+    std::string toString() const{
+        string ans = "";
+        charALNode * ptr = head;
+        while(!ptr){
+            ans += ptr -> nodeData.getValue();
+            ptr = ptr -> next;
+        }
+        return ans;
+    };
+    // ConcatStringList concat(const ConcatStringList & otherS) const;
+    // ConcatStringList subString(int from, int to) const;
+    // ConcatStringList reverse() const;
+    // ~ConcatStringList();
 
 public:
     class ReferencesList {
